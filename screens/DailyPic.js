4e5f171b4b83243a731c7ca62e0f32befff762e1
dannysophi17 <122616673+dannysophi17@users.noreply.github.com>
2023-10-11 21:35:24 -0500

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+export default class DailyPicScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Pantalla de Foto diaria</Text>
+
+            </View>
+        );
+    }
+}
